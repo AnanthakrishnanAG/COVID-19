a0c44e6deb021f18f7635b85ed15359851dfba49
Ananthakrishnan AG <56665596+AnanthakrishnanAG@users.noreply.github.com>
2020-09-22 12:13:59 +0530

    Update corona.py

--- corona.py
+++ corona.py
@@ -14,23 +14,23 @@ cor=Coronavirus()
 
 def corrun(cor):
 
-    cor.driver.get('https://www.worldometers.info/coronavirus/')
+    cor.driver.get('https://www.worldometers.info/coronavirus/') #website where we fetch the data about COVID-19 statitics
 
-    deaths=cor.driver.find_elements_by_xpath('//div[@class="maincounter-number"]')
+    deaths=cor.driver.find_elements_by_xpath('//div[@class="maincounter-number"]') # fetching covid death rate
     de=deaths[1]
     det=de.get_attribute("innerHTML")
 
     print(det[1])
-
-    account_sid = 'ACc4659e54f1c2e431d4be17db9acc7904'
-    auth_token = '714e7710fcc09eff284533ae465f2e92'
+# for GSM message I used a online messageing service twilio create an account at twilio and get your auth details here.
+    account_sid = 'ACc4659e54f1c2e431d4be**********' 
+    auth_token = '714e7710fcc09eff28453***********'
     client = Client(account_sid, auth_token)
 
     message = client.messages \
         .create(
         body=f"Hello this is krish    current corrona death is {det}, so be carefull and stay home",
-        from_='+12055089903',
-        to='+79629634124',
+        from_='+12055********', # this is the from number which twilio give to us for our account
+        to='+7962********', # number which we want to send automated message
 
     )
 
@@ -40,5 +40,5 @@ def corrun(cor):
 while True:
 
     corrun(cor)
-    time.sleep(60)
+    time.sleep(60) # give a time lag here appropriate to you. this decide the time interval which you get the message
 
